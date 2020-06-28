9f5f5bb84a5131ff7e7bca12868b03ea877c6477
nehadesaraju <66801479+nehadesaraju@users.noreply.github.com>
2020-06-27 22:14:06 -0500

    Increase scroll-reveal time

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ $(function() {
 
   window.sr = ScrollReveal({
     reset: false,
-    duration: 600,
+    duration: 800,
     easing: 'cubic-bezier(.694,0,.335,1)',
     scale: 1,
     viewFactor: 0.3,
